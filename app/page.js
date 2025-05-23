c125b7739469f350607c558bba2b5792d3047a3c
Tanvir <tanvir.info07@gmail.com>
2025-05-24 03:39:28 +0600

    Complete the project

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
-import DigitalAttestationResult from "@/components/DigitalAttestationResult";
 import LoginForm from "@/components/LoginForm";
-import Image from "next/image";
 
 export default function Home() {
   return (
