648436a77bf61327b9701c3b0b9fb3fe6607f675
Tanvir <tanvir.info07@gmail.com>
2025-05-24 03:38:43 +0600

    home page login form added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import DigitalAttestationResult from "@/components/DigitalAttestationResult";
+import LoginForm from "@/components/LoginForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="bg-[#f5f7fa]">
-      <DigitalAttestationResult />
+      <LoginForm />
     </main>
   );
 }
